8bd67f07e8bc3d88111e0ea885aacd3aa7005099
Henning Rauch <henning@ruppenmuehle.de>
2022-05-09 15:18:15 +0300

    bumped copyright

--- fallen-8-core-apiApp/Controllers/Model/StatusREST.cs
+++ fallen-8-core-apiApp/Controllers/Model/StatusREST.cs
@@ -2,7 +2,7 @@
 //
 // StatusREST.cs
 //
-// Copyright (c) 2021 Henning Rauch
+// Copyright (c) 2022 Henning Rauch
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
